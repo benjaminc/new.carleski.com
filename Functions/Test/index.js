2e903660f44508b168342af5094b06da6cd18615
Benjamin Carleski <benjamin@proworks.com>
2020-06-29 12:30:11 -0700

    Add check for chores input

--- Functions/Test/index.js
+++ Functions/Test/index.js
@@ -4,10 +4,11 @@ module.exports = async function (context, req) {
     const weekId = req && req.query ? req.query.weekId : null;
     const hasAuth = (req && req.headers && req.headers["x-ms-client-principal"]) ? true : false;
     const user = req && req.headers && req.headers["x-ms-client-principal"] ? JSON.parse(Buffer.from(req.headers["x-ms-client-principal"], "base64").toString("ascii")) : null;
-    const storageLen = typeof process.env.AzureWebJobsStorage === 'string' ? process.env.AzureWebJobsStorage.length : -1;
+    const storageLen = typeof process.env.RANDOM_STRING_NAME === 'string' ? process.env.RANDOM_STRING_NAME.length : -1;
+    const choresIn = context.bindings.choresIn;
 
     context.res = {
         status: 200,
-        body: JSON.stringify({hasReq, hasQuery, weekId, hasAuth, user, storageLen})
+        body: JSON.stringify({hasReq, hasQuery, weekId, hasAuth, user, storageLen, choresIn})
     };
 }
