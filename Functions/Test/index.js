84e92dfe8e885a34a106157d0b41aafcb9d37200
Benjamin Carleski <benjamin@proworks.com>
2020-06-29 11:26:50 -0700

    Update test function

--- Functions/Test/index.js
+++ Functions/Test/index.js
@@ -1,15 +1,13 @@
 module.exports = async function (context, req) {
-    const status = {
-        hasReq = !!req,
-        hasQuery = !!(req && req.query),
-        weekId = req && req.query ? req.query.weekId : null,
-        hasAuth = !!(req && req.headers && req.headers["x-ms-client-principal"]),
-        user = req && req.headers && req.headers["x-ms-client-principal"] ? JSON.parse(Buffer.from(req.headers["x-ms-client-principal"], "base64").toString("ascii")) : null,
-        storageLen = typeof process.env.AzureWebJobsStorage === 'string' ? process.env.AzureWebJobsStorage.length : -1
-    };
+    const hasReq = req ? true : false;
+    const hasQuery = (req && req.query) ? true : false;
+    const weekId = req && req.query ? req.query.weekId : null;
+    const hasAuth = (req && req.headers && req.headers["x-ms-client-principal"]) ? true : false;
+    const user = req && req.headers && req.headers["x-ms-client-principal"] ? JSON.parse(Buffer.from(req.headers["x-ms-client-principal"], "base64").toString("ascii")) : null;
+    const storageLen = typeof process.env.AzureWebJobsStorage === 'string' ? process.env.AzureWebJobsStorage.length : -1;
 
     context.res = {
         status: 200,
-        body: JSON.stringify(status)
+        body: JSON.stringify({hasReq, hasQuery, weekId, hasAuth, user, storageLen})
     };
 }
