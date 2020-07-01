d1a2a7bd007c729056b6855933c3c3076c182fc0
Benjamin Carleski <benjamin@proworks.com>
2020-07-01 08:22:42 -0700

    Register service worker

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
 
 Vue.config.productionTip = false
 
