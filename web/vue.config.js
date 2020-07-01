28a878ec1260e2de81c7e765a99bfe539dc739b4
Benjamin Carleski <benjamin@proworks.com>
2020-07-01 08:05:55 -0700

    Add PWA

--- web/vue.config.js
+++ web/vue.config.js
@@ -11,4 +11,23 @@ module.exports = {
         .use('vue-svg-loader')
         .loader('vue-svg-loader')
     },
+    pwa: {
+      name: "Chore Chart",
+      themeColor: "#008000",
+      msTileColor: "#404040",
+      appleMobileWebAppCapable: 'yes',
+      manifestPath: "site.webmanifest",
+      manifestOptions: {
+        name: "Carleski Family Chore Chart",
+        background_color: "#ffffff",
+        start_url: '/'
+      },
+      iconPaths: {
+        favicon32: '/img/favicon-32x32.png',
+        favicon16: '/img/favicon-16x16.png',
+        appleTouchIcon: '/img/apple-touch-icon.png',
+        maskIcon: '/img/maskable_icon.png',
+        msTileImage: '/img/android-chrome-192x192.png'
+      }
+    }
   }
